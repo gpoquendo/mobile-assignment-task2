be5f9ad69a1c3271ba398f98271c39011703d0b3
gpoquendo <againstthaflow13@protonmail.com>
2024-11-22 11:55:07 -0700

    Update release workflow to create GitHub release and use environment variable for API_URL

--- app.config.ts
+++ app.config.ts
@@ -43,7 +43,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     eas: {
       projectId: "422959f3-7014-406d-92d3-e44fb1608cae"
     },
-    API_URL: "https://my-json-server.typicode.com/gpoquendo/mobile-assignment-task2",
+    API_URL: process.env.API_URL,
     IMGBB_API_KEY: process.env.IMGBB_API_KEY,
   },
 });
